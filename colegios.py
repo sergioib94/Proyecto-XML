a8a0f788c37950ac5412b73ab7a0b61bac60a8aa
Sergio Ibañez <sergio_hd_sony@hotmail.com>
2020-02-21 17:38:12 +0100

    Ejercicio 4 hecho

--- colegios.py
+++ colegios.py
@@ -2,6 +2,10 @@ def Listar (doc):
     lista = doc.xpath('//Centro/text()')
     return lista
 
+def Buscar (pedania,doc):
+    pedanias = doc.xpath ('//Localizacion[Pedania="%s"]/../Centro/text()' %pedania)
+    return pedanias
+
 from lxml import etree
 doc = etree.parse ('colegios_lorca.xml')
 
@@ -14,6 +18,7 @@ while True:
     print ("3.Buscar o Filtrar informacion: Meter por teclado un nombre o parte de el y mostrar los contactos de esos colegios.")
     print ("4.Buscar informacion relacionada: Introducir por teclado una pedania y decir que colegio hay en esa pedania.")
     print ("5.Ejercicio Libre: Meter el nombre de un colegio o bien el nombre de un parque y usando las coordenadas y las coordenadas de un segundo fichero(parques de lorca), mostrar que parques estan cerca de algun colegio o que colegios tienen parques cerca.")
+    print ("6.Ayuda XML (listado de pedanias)")
     print ("0.Salir")
     print ("")
 
@@ -21,8 +26,18 @@ while True:
 
     if opcion == "1":
         print ("Listado de los colegios: ")
+        print ("")
         for colegios in Listar (doc):
             print ("*",colegios)
+
+    if opcion == "4":
+        pedania = input("Introduce una pedania: ")
+
+        print ("Colegios de la pedania: ")
+        print ("")
+
+        for pedanias in Buscar (pedania,doc):
+            print ("*",pedanias)
 
     if opcion == "0":
         break;
